e685e7a3a9ec24f89beffb107f8efa17d3b2fbe2
atomicsaiko <atomicsaiko@gmail.com>
2017-10-10 16:40:17 +0200

    Add Feathers classes service

--- src/services/index.js
+++ src/services/index.js
@@ -2,9 +2,12 @@ const students = require('./students/students.service.js');
 const scores = require('./scores/scores.service.js');
 const users = require('./users/users.service.js');
 
+const classes = require('./classes/classes.service.js');
+
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(students);
   app.configure(scores);
   app.configure(users);
+  app.configure(classes);
 };
